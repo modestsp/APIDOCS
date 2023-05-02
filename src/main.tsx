f2e70918e2d161a6e528a6bb995135d75982f98d
srsebbi <csperichon@gmail.com>
2023-05-02 20:27:33 -0300

    Added react router and router for category, user, product and review

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Layout from './Components/Layout/Layout.tsx';
+import Users from './pages/Users.tsx';
+import Products from './pages/Products.tsx';
+import Categories from './pages/Category.tsx';
+import Reviews from './pages/Reviews.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout><App /></Layout>
+  },
+  {
+    path: "/users",
+    element: <Layout><Users /></Layout>
+  },
+  {
+    path: "/products",
+    element: <Layout><Products /></Layout>
+  },
+  {
+    path: "/categories",
+    element: <Layout><Categories /></Layout>
+  },
+  {
+    path: "/reviews",
+    element: <Layout><Reviews /></Layout>
+  }
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
