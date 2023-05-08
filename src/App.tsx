4bf7f9db66627b8ad774f069fc8377ccdb49e3b4
srsebbi <csperichon@gmail.com>
2023-05-08 19:38:36 -0300

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import './App.css'
-import DisplayCode from './Components/DisplayCode'
-import Header from './Components/Header'
-import Sidebar from './Components/Sidebar'
-import Footer from './Components/Footer'
 import styles from "../src/styles/DocsPage.module.css"
 
 function App() {
 
   return (
-    <main className={styles.DocsPage}
-    >
-      <Header />
-      <div className={styles.mainContent}>
-        <Sidebar />
-        <DisplayCode />
-      </div>
-      <Footer />
-    </main>
+    <div className={styles.mainContent}>
+      {/* Hello: */}
+    </div>
+
   );
 }
 
-export default App
+// export default App
+
